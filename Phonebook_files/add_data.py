e8eedf07641f52cb658877ba10e1ba383e486c3d
Nata Kregel <natik.kregel@gmai.com>
2024-03-30 16:32:16 +0300

    Телефонный справочник

---
+++ Phonebook_files/add_data.py
@@ -0,0 +1,16 @@
+from return_data import data_file
+
+
+def add_row(new = True, row_to_data = [], data = [], nf = 1):
+    if new:
+        row_to_data.append(input("Введите свое имя: ") + ';')
+        row_to_data.append(input("Введите свою фамилию: ") + ';')
+        row_to_data.append(input("Введите дату рождения: ") + ';')
+        row_to_data.append(input("Введите город: ") + "\n")
+        data, nf = data_file()
+    
+    now_number_row = len(data) + 1
+    row_to_data.insert(0, (str(now_number_row) + ';'))    
+    with open(f'db/data_{nf}.txt', 'a', encoding='utf-8') as file:
+        file.write("".join(row_to_data))
+    print("Данные успешно записаны!")
